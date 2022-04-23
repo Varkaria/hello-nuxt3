107edb2c821b210765fa4161ab76e73b1942f07a
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2022-04-24 00:30:47 +0700

    🙂 sup

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
-        yo WOOOOOOOOOO
+        <div class="w-64 bg-gray-400 h-40"></div>
+        <div class="w-64 bg-gray-400 h-40"></div>
+        <div class="w-64 bg-gray-400 h-40"></div>
     </div>
 </template>
